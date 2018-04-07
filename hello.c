95ad492aa60adc1c9a3f9310e6faeaff9162e141
shapshai2 <38151726+shapshai2@users.noreply.github.com>
2018-04-07 19:31:56 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@ int main()
 char *s="Shai Changed new version";
 
 printf("Hello world. this change should be picked up by the Jenkins PR job\n");
+printf("I hope to get this automatically tested.\n");
 
 printf(s);
   return(0);
